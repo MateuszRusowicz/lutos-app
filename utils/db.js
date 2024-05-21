abe3da5ca07143c5791c5c0c16b9d2c467785548
MateuszRusowicz <130058955+MateuszRusowicz@users.noreply.github.com>
2024-05-21 16:15:44 +0200

    first commit

---
+++ utils/db.js
@@ -0,0 +1,6 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+export async function openDb() {
+  return open({ filename: "./lutos-app.db", driver: sqlite3.Database });
+}
---
+++ utils/dbInit.js
@@ -0,0 +1,24 @@
+import { openDb } from "./db.js";
+
+const dbSetup = async function () {
+  const db = await openDb();
+  await db.exec(`
+  create table if not exists compositions(
+    id int auto_increment primary key,
+    composer varchar(100),
+    title varchar(100)
+  );
+  create table if not exists musicians(
+    id int auto_increment primary key,
+    name varchar(100)
+  );
+  create table if not exists compositions_musicians(
+    composition_id int,
+    musician_id int,
+    foreign key (composition_id) references compositions(id),
+    foreign key (musician_id) references musicians(id)
+  );`);
+  console.log("performed successfully");
+};
+
+dbSetup();
