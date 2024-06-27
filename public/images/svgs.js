239b86723646abfa6bd7e09ca688d49f5f0abae0
MateuszRusowicz <130058955+MateuszRusowicz@users.noreply.github.com>
2024-06-27 08:07:33 +0200

    refactored site by migrating svg's to separate file

--- public/images/svgs.js
+++ public/images/svgs.js
@@ -1,10 +1,48 @@
-export default function Spinner() {
+export function PlusIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  );
+}
+
+export function ScheduleIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+      />
+    </svg>
+  );
+}
+
+export function SpinnerIcon() {
   return (
     <svg
       width="512"
       height="512"
       viewBox="0 0 512 512"
-      style={{ color: "currentColor" }}
+      style={{ color: "#ffffff" }}
       xmlns="http://www.w3.org/2000/svg"
       className="h-full w-full"
     >
@@ -21,18 +59,18 @@ export default function Spinner() {
         paint-order="stroke"
       ></rect>
       <svg
-        width="256px"
-        height="256px"
+        width="120px"
+        height="120px"
         viewBox="0 0 24 24"
-        fill="currentColor"
-        x="128"
-        y="128"
+        fill="#ffffff"
+        x="196"
+        y="196"
         role="img"
         style={{ display: "inline-block", verticalAlign: "middle" }}
         xmlns="http://www.w3.org/2000/svg"
       >
         <g fill="currentColor">
-          <circle cx="12" cy="12" r="0" fill="currentColor">
+          <circle cx="12" cy="12" r="0" fill="#ffffff">
             <animate
               id="svgSpinnersPulse30"
               fill="freeze"
@@ -53,11 +91,12 @@ export default function Spinner() {
               values="1;0"
             />
           </circle>
-          <circle cx="12" cy="12" r="0" fill="currentColor">
+          <circle cx="12" cy="12" r="0" fill="#ffffff">
             <animate
               id="svgSpinnersPulse31"
               fill="freeze"
               attributeName="r"
+              svg
               begin="svgSpinnersPulse30.begin+0.4s"
               calcMode="spline"
               dur="1.2s"
@@ -74,7 +113,7 @@ export default function Spinner() {
               values="1;0"
             />
           </circle>
-          <circle cx="12" cy="12" r="0" fill="currentColor">
+          <circle cx="12" cy="12" r="0" fill="#ffffff">
             <animate
               id="svgSpinnersPulse32"
               fill="freeze"
@@ -100,3 +139,5 @@ export default function Spinner() {
     </svg>
   );
 }
+
+export function ErrorIcon() {}
