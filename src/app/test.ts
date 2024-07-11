fc00851830ed9691ff014b2aed127fdad38c3510
MateuszRusowicz <130058955+MateuszRusowicz@users.noreply.github.com>
2024-07-11 14:41:09 +0200

    refactoring

--- src/app/test.ts
+++
@@ -1,9 +0,0 @@
-const highScore: number | boolean = 23;
-const stuff: number[] | string[] = [];
-let skillLevel: "Beginner" | "advanced" | "expert";
-type SkiSchoolStudent = {
-  name: string;
-  age: number;
-  sport: "ski" | "snowboard";
-  level: typeof skillLevel;
-};
